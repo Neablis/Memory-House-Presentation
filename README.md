5c2858ef3ca05ab5f01aaf5b02d454c5547d7c68
Mitchell DeMarco <Neablis121@gmail.com>
2012-04-27 14:35:46 -0400

    Init

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Memory-House-Presentation
 =========================
 
 The HTML5 Dynamic presentation portion of memory-House
+
+Uses canvas to map photos and video to the screen and play them dynamically in time to a musical performance.
+Also maps text messages in real time to the presentation. 
---
+++ ajax/tag.php
@@ -0,0 +1,22 @@
+<?php
+
+  $con = mysql_connect("cias.rit.edu","nostalgia","miniM3K0dom");
+
+ if (!$con){
+        exit_status('Could not connect');
+      }else{
+        mysql_select_db("nostalgia", $con);
+        
+        $result = mysql_query("SELECT * FROM images  WHERE Approved = '1' ORDER BY RAND() LIMIT 0,1;");
+        $row = mysql_fetch_array($result);
+      }
+      
+      mysql_close($con);
+	  $return = array(
+      "message" => $row['message']
+		);
+		
+		//echo print_r($return, true);
+		echo json_encode($return);
+
+?>
